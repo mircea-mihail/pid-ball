a339c0691af55cb6ffed869f1fdc083ba0841039
Ionescu Mircea Mihail <mihail.mircea12@gmail.com>
2023-02-10 01:11:07 +0200

    proportional part kind of done. It's on the brink of being unstable so should be good for receiving the integrator and derivator

--- pid-ball.ino
+++ pid-ball.ino
@@ -1,14 +1,37 @@
+#include <SR04.h>
 #include <Servo.h>
-Servo servo;
+
 const int pot = 0;
+const int ultrasonicEcho = 11, ultrasonicTrig = 12;
+
+Servo servo;
+SR04 sensor = SR04(ultrasonicEcho, ultrasonicTrig);
+
+const int maxh = 66, minh = 153, midh = 127;
+const int reactionTime = 20, conversionVal = 5.64;
+const int minl = 0, maxl = 46;
+const int avgRate = 5; 
+int v[avgRate];
+
+void getInitialV(){
+  for(int i = 0; i < avgRate; i++){
+    long int distance = sensor.Distance();
+    if(distance > 50)
+      v[i] = 50;
+    else 
+      v[i] = distance; 
+  }
+}
 
 void setup() {
   Serial.begin(9600);
   pinMode(pot, INPUT);
   servo.attach(9);
+  servo.write(midh);
+  getInitialV();
 }
-const int maxh = 70, minh = 156;
-const int reactionTime = 20, conversionVal = 5.64;
+
+// minimum length = 3, maximum length = 46; (distances from the sensor)
 
 void minMax(){
   servo.write(minh);
@@ -18,9 +41,84 @@ void minMax(){
 }
 
 void potManualControl(){
-  servo.write(analogRead(pot)/conversionVal);  
+  servo.write(analogRead(pot)/conversionVal);
+}
+
+double y; //raw output
+double r; //refference
+double e; //the error (r - y)
+
+double avgV(){
+  double sum = 0;
+  for(int i = 0; i < avgRate; i++){
+    sum += v[i];
+  }
+  return sum/avgRate;
+}
+
+double midl(){
+  return (maxl + minl)/2;
+}
+
+double errorFunction(double e){
+  e = abs(e);
+  if(e > 20){
+    return 1;
+  }
+  else return (e / 20.0);
+}
+
+int vcounter = 0;
+
+void updateV(){
+  if(vcounter == avgRate){
+    vcounter = 0;
+  }
+  long int distance = sensor.Distance();
+  if(distance > 50)
+    v[vcounter] = 50;
+  else 
+    v[vcounter] = distance; 
+  vcounter ++;
+}
+
+double servoSafety(double val){
+  if(val < minh){
+    return minh;
+  }
+  
+  if(val > maxh){
+    return maxh;
+  }
+
+  return val;
+}
+
+const int acceptedError = 1;
+
+double feedBack(){
+  if(e > acceptedError){
+    //left side
+    return midh - (midh-maxh) * errorFunction(e);
+  }
+  else if(e < -acceptedError){
+    //right side
+    return midh - (midh-minh) * errorFunction(e);
+  }
+  else{
+    return midh;
+  }
 }
 
 void loop() {
-  
+
+  updateV();
+  y = avgV();
+  r = midl();
+  e = r - y;
+
+  servo.write(feedBack());
+  // servo.write(127);
+  // potManualControl();
+  // Serial.println(analogRead(pot)/conversionVal);
 }
