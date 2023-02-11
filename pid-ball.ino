c31f4613245c35ab2722059416f0967f8db22a5b
Ionescu Mircea Mihail <mihail.mircea12@gmail.com>
2023-02-11 02:01:54 +0200

    better y readings with the delay, better idea to implement for the derivator

--- pid-ball.ino
+++ pid-ball.ino
@@ -10,7 +10,7 @@ SR04 sensor = SR04(ultrasonicEcho, ultrasonicTrig);
 const int maxh = 66, minh = 153, midh = 127;
 const int reactionTime = 20, conversionVal = 5.64;
 const int minl = 0, maxl = 46;
-const int avgRate = 10; 
+const int avgRate = 100; 
 int v[avgRate];
 
 void getInitialV(){
@@ -44,8 +44,13 @@ void potManualControl(){
   servo.write(analogRead(pot)/conversionVal);
 }
 
+double midl(){
+  return (maxl + minl)/2;
+}
+
+double integratorVal; //integrator value
 double y; //raw output
-double r; //refference
+double r = midl(); //refference
 double e; //the error (r - y)
 
 double avgV(){
@@ -54,10 +59,6 @@ double avgV(){
     sum += v[i];
   }
   return sum/avgRate;
-}
-
-double midl(){
-  return (maxl + minl)/2;
 }
 
 double errorFunction(double e){
@@ -96,37 +97,77 @@ double servoSafety(double val){
 
 const int acceptedError = 2;
 
-double feedBack(){
+double feedBack(double e){
   if(e > acceptedError){
     //left side
-    return midh - (midh-maxh) * errorFunction(e);
+    return - (midh-minh) * errorFunction(e);
+
   }
   else if(e < -acceptedError){
     //right side
-    return midh - (midh-minh) * errorFunction(e);
+    return - (midh-maxh) * errorFunction(e);
   }
   else{
-    return midh;
+    return 0;
+  }
+}
+
+double integrator(double e){
+  if(e > acceptedError){
+    //left side
+    return - (midh-maxh) * errorFunction(e);
+  }
+  else if(e < -acceptedError){
+    //right side
+    return - (midh-minh) * errorFunction(e);
+  }
+  else{
+    return 0;
   }
 }
 
 const int derivatorFactor = 0.125;
-double derivator(){
-  return e * 0.2;
+int derivator(){
+  return v[avgRate] - v[0];
+}
+
+void checkServo(){
+  for(int i = minh; i > maxh; i--){
+    servo.write(i);
+    Serial.println(sensor.Distance());
+    delay(100);
+  }
+
+  for(int i = maxh; i < minh; i++){
+    servo.write(i);
+    Serial.println(sensor.Distance());
+    delay(100);
+  }
 }
 
 void loop() {
-
-  updateV();
   //though averageing the y might actually make my 
   //feedback an integrator as averaging it makes it take into account previous information.
   //as such i might end up not using a proportional part as it would be too twitchy?
   //not sure, will see after adding a derivator
-  y = avgV();
-  r = midl();
-  e = r - y;
+  
+  //the problem is that the sensor goes crazy when the motor does stuff, so i figured i would give it a delay, to allow the 
+  //servo to do its thing and then update the position and it seemed to work. The problem is the derivator is really not helping out.
+  
+  //the oscilation of the "ball" seems to amplify instead of fizzle out so i'll see what i can do 
+  delay(10);
 
-  servo.write(feedBack() + derivator());
+  integratorVal = avgV();
+  updateV();
+  y = sensor.Distance();
+
+  //0.6 feedback 0.4 integrator 0.125 derivator and add everything up to midh
+
+  //not sure if i made it better or worse
+
+  servo.write(midh + feedBack(r - y) + 0.125 *  derivator());
+
+  //we'll see tomorrow how this goes
   // servo.write(127);
   // potManualControl();
   // Serial.println(analogRead(pot)/conversionVal);
